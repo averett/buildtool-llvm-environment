affb9fc282433ffc339a61968a60ece23e8d0dfd
John <john@johnwaller.us>
2022-01-10 04:52:50 -0600

    Don't know which direction to go here...

--- toolchain.sh
+++ toolchain.sh
@@ -1,4 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 
-cat $1.cmake
-echo 'export TARGET=<your_target_tuple> # EXPORT dumbass export...' >2
+show_help() {
+	echo "toolchain <target triple> <rootfs path>"
+}
+show_error() {
+	1>&2 echo $@
+}
+
+toolchain() {
+	if [[ -z "$1" ]]; then
+		show_help
+		exit 1
+	fi
+
+	# split tuple
+	local tuple=(${1//-/ })
+	if (( ${#tuple[@]} != 4 )); then
+		show_help
+		show_error 'invalid target triple'
+		exit 2
+	fi
+
+	if [[ -z "$2" ]]; then
+		show_help
+		show_error 'must pass rootfs directory'
+		exit 1
+	fi
+
+	local sysroot=''
+	for i in "$2" "${2}/${1}"; do
+		if [[ -d $i ]]; then
+			sysroot=$(cd $i && pwd)
+		fi
+	done
+	
+	if [[ -z "$sysroot" ]]; then
+		show_help
+		show_error 'sysroot could not be found'
+		exit 1
+	fi
+
+	local arch=${tuple[0]}
+	local vendor=${tuple[1]}
+	local sys=${tuple[2]}
+	local abi=${tuple[3]}
+
+	local system=''
+	if [[ "$sys" == 'linux' ]]; then
+		system='Linux'
+	elif [[ "$sys" == 'windows' ]]; then
+		system='Windows'
+	fi
+
+	sed -r \
+		-e "s/(CMAKE_SYSTEM_PROCESSOR )[^\)]*/\1${arch}/" \
+		-e "s/(CMAKE_SYSTEM_NAME )[^\)]*/\1${system}/" \
+		-e "s/(CMAKE_C(XX)?_COMPILER_TARGET )[^\)]*/\1${1}/" \
+		< /tuple.cmake
+
+	printf 'set(CMAKE_SYSROOT "%s")\n' "${sysroot}"
+}
+toolchain $@
+
